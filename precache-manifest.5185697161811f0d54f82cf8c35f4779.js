2aeea3782d4ea4db810d57960c6b18f417c515e0
Sudarshan Saxena <sudarshansaxena@Sudarshans-MacBook-Air.local>
2020-02-17 22:05:32 +0530

    Updates

---
+++ precache-manifest.5185697161811f0d54f82cf8c35f4779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a231c30339a3dea69f57ebc16f42a91",
+    "url": "/pic-fetch/index.html"
+  },
+  {
+    "revision": "d8712d2b0ab5482ce6c4",
+    "url": "/pic-fetch/static/css/main.71a8ce95.chunk.css"
+  },
+  {
+    "revision": "6857949fa2f6c207ced3",
+    "url": "/pic-fetch/static/js/2.3801cada.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pic-fetch/static/js/2.3801cada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8712d2b0ab5482ce6c4",
+    "url": "/pic-fetch/static/js/main.02738647.chunk.js"
+  },
+  {
+    "revision": "c4b36b1286f1986ee78a",
+    "url": "/pic-fetch/static/js/runtime-main.a4898061.js"
+  }
+]);
